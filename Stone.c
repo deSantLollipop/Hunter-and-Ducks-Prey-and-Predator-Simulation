86ffb1d328c0a4f3ee0c9092ccc3cf118a33b9f5
Ivan <35543935+deSantLollipop@users.noreply.github.com>
2018-06-01 15:07:59 +0200

    Update Stone.c

--- Stone.c
+++ Stone.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
+#include <stdbool.h>//without this my visual studio don't want to recognise normaly 'bool'
+
 //There is lake, where are ducks and huter, simulation of hunting game.
-struct Lake
 {
 	int a;
 };
@@ -13,7 +13,7 @@ struct Duck //ducks has health, ability to fly away and damage they get
 	bool flyable;
 	int hp;
 	int getdamage;
-	struct duck *next;
+	struct duck *next;// somehow i have to create a list or massive of ducks, and fill it randomly
 }first;
 
 struct Hunter //hunter has his own health and damage
@@ -22,7 +22,7 @@ struct Hunter //hunter has his own health and damage
 	int damage;
 };
 
-struct Duck MallardDuck = {true,100,0}; 
+struct Duck MallardDuck = {true,100,0}; //each type of ducks have their parameters
 struct Duck RedHatDuck  = {true,120,0};
 struct Duck RubberDuck  = {false, 200, 20};
 
@@ -30,7 +30,7 @@ struct Duck RubberDuck  = {false, 200, 20};
 
 int main()
 {
-	//struct Duck mas[]
+	//struct Duck mas[] or some list
 
 
 sustem("pause");

18674ef6d38aa52fe195dc95608721ca170264fe
Ivan <35543935+deSantLollipop@users.noreply.github.com>
2018-06-01 15:04:12 +0200

    Update Stone.c

--- Stone.c
+++ Stone.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-
+//There is lake, where are ducks and huter, simulation of hunting game.
 struct Lake
 {
 	int a;
 };
 
-struct Duck
+struct Duck //ducks has health, ability to fly away and damage they get
 {
 	bool flyable;
 	int hp;
@@ -16,13 +16,13 @@ struct Duck
 	struct duck *next;
 }first;
 
-struct Hunter
+struct Hunter //hunter has his own health and damage
 {
 	int hp;
 	int damage;
 };
 
-struct Duck MallardDuck = {true,100,0};
+struct Duck MallardDuck = {true,100,0}; 
 struct Duck RedHatDuck  = {true,120,0};
 struct Duck RubberDuck  = {false, 200, 20};
 

57ba930d518df17217fd522e064072f3368a0699
Ivan <35543935+deSantLollipop@users.noreply.github.com>
2018-06-07 15:29:02 +0200

    Update Stone.c

--- Stone.c
+++ Stone.c
@@ -4,36 +4,62 @@
 #include <stdbool.h>//without this my visual studio don't want to recognise normaly 'bool'
 
 //There is lake, where are ducks and huter, simulation of hunting game.
-{
-	int a;
-};
+//{
+//	int a;
+//};
 
-struct Duck //ducks has health, ability to fly away and damage they get
-{
-	bool flyable;
-	int hp;
-	int getdamage;
-	struct duck *next;// somehow i have to create a list or massive of ducks, and fill it randomly
-}first;
-
-struct Hunter //hunter has his own health and damage
+struct Hunter 
 {
 	int hp;
 	int damage;
 };
 
-struct Duck MallardDuck = {true,100,0}; //each type of ducks have their parameters
-struct Duck RedHatDuck  = {true,120,0};
-struct Duck RubberDuck  = {false, 200, 20};
+struct Duck {
+	bool flyable;
+	int hp;
+	int getdamage;	
+	/*struct Duck *next;*/
+}/**first*/;
+
+	struct Duck MallardDuck = {true,100,0}; 
+	struct Duck RedHatDuck  = {true,120,0};
+	struct Duck RubberDuck  = {false, 200, 20};
 
 
+void DuckList()
+{
+
+	int i, typd;
+	int numd = rand() %50;
+
+	struct Duck ducks[50];
+
+
+	for(i=0; i<numd; i++)
+	{
+	//	ducks[i] = {};
+	}
+
+	//struct Duck *tmpf=first;
+    //for(i=1; i<list_size; i++){
+	//struct Duck *temp=(struct Duck *)malloc(sizeof(struct Duck));
+	//temp->n=liczba;
+	//tmpf->next=temp;
+    //temp->next=NULL;
+    //tmpf=temp;}
+
+}
 
 int main()
 {
+	srand(time(NULL));
 	//struct Duck mas[] or some list
 
 
 system("pause");
 return 0;
 }
+
+
+
 
